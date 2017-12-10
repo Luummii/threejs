7ab483a3616bec24e44a3eab98d4b643f9d3f4db
Andrey Zimin (luummii) <luummii@vuejs.fun>
2017-12-10 22:49:52 +0300

    Deploy in Heroku

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template lang="html">
-  <div id="app"> 
-    <div class="top">
-      <img class="logo" src="./assets/img/logo.png">
-      <p>Vue-starter-kit - the template for fast start your app.</p> 
-      <app-menu></app-menu>
-    </div>      
+  <div id="app">       
     <router-view></router-view>
   </div>
 </template>
@@ -23,26 +18,6 @@ export default {
 <style lang="stylus">
 @import './assets/lib/normalize'
 
-@font-face
-  font-family 'Ubuntu-Bold'
-  src url('./assets/fonts/Ubuntu-Bold.woff2') format('woff2')
-  font-weight bold
-  font-style normal  
-
-body
-  font-family Ubuntu-Bold
-  font-size 30px
-
-.top
-  position relative
-  background-color #E3E1E9
-  height 340px
-
-.logo
-  width 200px
-  height 200px
-  display block
-  margin 0 auto
-p 
-  text-align center
+html, body
+ overflow hidden 
 </style>
